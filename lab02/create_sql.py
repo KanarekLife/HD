eb826763fda9393170c8a827ce53224519dc5992
ketrab <bartekkrawisz@wp.pl>
2024-10-27 20:22:33 +0100

    next update

--- lab02/create_sql.py
+++ lab02/create_sql.py
@@ -10,7 +10,7 @@ def chunk(it, size):
 first_dump = Path('data/first_dump')
 second_dump = Path('data/second_dump')
 
-result_file = second_dump / 'apply.sql'
+result_file = str(second_dump / '{}_apply_{}.sql')
 
 csv_by_table = {
     'Egzaminatorzy': 'egzaminatorzy.csv',
@@ -20,36 +20,36 @@ csv_by_table = {
     'ZaplanowanePytania': 'zaplanowane_pytania.csv',
 }
 
-with open(result_file, 'w') as f:
-    for table, csv in csv_by_table.items():
-        first = pd.read_csv(first_dump / csv, encoding='utf-16')
-        second = pd.read_csv(second_dump / csv, encoding='utf-16')
 
-        print(f'Processing {table}...')
+for i, (table, csv) in enumerate(csv_by_table.items()):
+    first = pd.read_csv(first_dump / csv, encoding='utf-16')
+    second = pd.read_csv(second_dump / csv, encoding='utf-16')
 
-        if 'Id' in first.columns and table != 'ZaplanowanePytania':
-            # find rows with same Id but different data
-            diff = first.merge(second, on='Id', suffixes=('_first', '_second'))
-            diff = diff[~diff.eq(diff.shift(-1, axis=1)).all(axis=1)]
-            # convert difference to SQL UPDATEs
-            prompt = f'UPDATE {table} SET\n'
-            updates = ''
-            for _, row in diff.iterrows():
-                for col, dt in zip(first.columns, first.dtypes):
-                    if col == 'Id':
-                        continue
-                    if not pd.isna(row[f'{col}_second']) and row[f'{col}_first'] != row[f'{col}_second']:
-                        formatted_val = f"{int(row[f'{col}_second'])}" if str(dt) in ('int64', 'float64') else f"'{row[f'{col}_second']}'"
-                        updates += f'UPDATE {table} SET {col} = {formatted_val} WHERE Id = \'{row["Id"]}\';\n'
+    print(f'Processing {table}...')
 
-        # convert difference to SQL INSERTs
-        diff = second[~second.isin(first)].dropna()
-        prompt = f'INSERT INTO {table}({", ".join(first.columns)}) VALUES\n'
-        inserts = [prompt + ',\n'.join(['(' + ', '.join([f"{int(v)}" if str(dt) in ('int64', 'float64') else f"'{v}'" for v, dt in zip(row, first.dtypes)]) + ')' for _, row in chunk]) for chunk in chunk(diff.iterrows(), 1000)]
-        inserts = ';\n'.join(inserts)
+    # create updates
+    if 'Id' in first.columns and table != 'ZaplanowanePytania':
+        diff = first.merge(second, on='Id', suffixes=('_first', '_second'))
+        diff = diff[~diff.eq(diff.shift(-1, axis=1)).all(axis=1)]
+        prompt = f'UPDATE {table} SET\n'
+        updates = ''
+        for _, row in diff.iterrows():
+            for col, dt in zip(first.columns, first.dtypes):
+                if col == 'Id':
+                    continue
+                if not pd.isna(row[f'{col}_second']) and row[f'{col}_first'] != row[f'{col}_second']:
+                    formatted_val = f"{int(row[f'{col}_second'])}" if str(dt) in ('int64', 'float64') else f"'{row[f'{col}_second']}'"
+                    updates += f'UPDATE {table} SET {col} = {formatted_val} WHERE Id = \'{row["Id"]}\';\n'
 
-        f.write(inserts)
+    # create inserts
+    diff = second[~second.isin(first)].dropna()
+    prompt = f'INSERT INTO {table}({", ".join(first.columns)}) VALUES\n'
+    inserts = [prompt + ',\n'.join(['(' + ', '.join([f"{int(v)}" if str(dt) in ('int64', 'float64') else f"'{v}'" for v, dt in zip(row, first.dtypes)]) + ')' for _, row in chunk]) for chunk in chunk(diff.iterrows(), 1000)]
+    inserts = ';\n'.join(inserts)
+
+    with open(result_file.format(i+1, table), 'w', encoding='utf-16') as f:
+        f.write(updates)
         if inserts and updates:
             f.write('\n')
-        f.write(updates)
+        f.write(inserts)
         f.write('GO\n\n')
